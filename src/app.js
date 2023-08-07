70ba519294e4c48d30667f2675ade66f92b8cf6c
araz0515 <arazkubtanjian@gmail.com>
2023-08-07 02:38:19 -0400

    Added functionality for the Select

--- src/app.js
+++ src/app.js
@@ -34,3 +34,26 @@ function setDate() {
 }
 setDate();
 setInterval(setDate, 1000);
+
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+
+  if (cityTimezone.length > 0) {
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+   <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss [<small>]A [</small>]"
+          )}</div>
+        </div>`;
+  }
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
