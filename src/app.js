369e51bfb8cba2857ca050a4fecfdad66abbade8
araz0515 <arazkubtanjian@gmail.com>
2023-08-07 01:43:05 -0400

    integration of moment.js for date and time

---
+++ src/app.js
@@ -0,0 +1,33 @@
+function setDate() {
+  let losAngelesDateElement = document.querySelector("#los-angeles .date");
+  losAngelesDateElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("MMMM Do YYYY");
+
+  let losAngelesTimeElement = document.querySelector("#los-angeles .time");
+  losAngelesTimeElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("h:mm:ss [<small>]A [</small>]");
+
+  let torontoDateElement = document.querySelector("#toronto .date");
+  torontoDateElement.innerHTML = moment()
+    .tz("America/Toronto")
+    .format("MMMM Do YYYY");
+
+  let torontoTimeElement = document.querySelector("#toronto .time");
+  torontoTimeElement.innerHTML = moment()
+    .tz("America/Toronto")
+    .format("h:mm:ss [<small>]A[</small>]");
+
+  let parisDateElement = document.querySelector("#paris .date");
+  parisDateElement.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("MMMM Do YYYY");
+
+  let parisTimeElement = document.querySelector("#paris .time");
+  parisTimeElement.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("h:mm:ss [<small>]A [</small>]");
+}
+
+setInterval(setDate, 1000);
